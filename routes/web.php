95da216db20bebf915121d4c39d80fa52351e541
duchung <tdhabcdef1710@gmail.com>
2024-11-01 13:41:43 +0700

    add shop-grid, shopping-cart

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+
+Route::get('/shop-grid', function () {
+    return view(('shop-grid'));
+})->name('shop-grid');
+
+Route::get('/shopping-cart', function () {
+    return view(('shopping-cart'));
+})->name('shopping-cart');
